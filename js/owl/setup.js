7369dc4e71100083b7a48e80fb1bf1d2bfdaefbe
VSF <47865897+HashiY@users.noreply.github.com>
2022-01-25 00:59:39 -0300

    Modal

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -1,7 +1,7 @@
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
-    nav:true,
+    nav:false,
     responsive:{
         0:{
             items:1
@@ -14,3 +14,27 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+// Modal
+var modal = document.getElementById("myModal"); // Pega
+
+var btn = document.getElementById("myBtn");//botao do modal
+
+var span = document.getElementsByClassName("close")[0];//<span> que fecha o modal
+
+// Abre o modal clicando no botao
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+//  <span> (x), fecha o modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Se clica fora do modal, fecha 
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
